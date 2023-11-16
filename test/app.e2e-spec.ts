bf6977d8c0bc173dc893569a0820d7e3b7885ae1
David Yoon <qqpo12@naver.com>
2023-11-16 16:03:41 +0900

    Add e2e test cases
    * Added a patch test case
    * Added a delete test case

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -34,7 +34,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -45,7 +45,19 @@ describe('AppController (e2e)', () => {
         .expect(201);
     });
 
-    it('DELETE', () => {
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'super',
+          year: 2002,
+          genres: ['test'],
+          other: 'Thing',
+        })
+        .expect(400);
+    });
+
+    it('DELETE 404', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
   });
@@ -57,7 +69,18 @@ describe('AppController (e2e)', () => {
     it('GET 404', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('UPDATE');
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({
+          title: 'Superman3',
+        })
+        .expect(200);
+    });
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
