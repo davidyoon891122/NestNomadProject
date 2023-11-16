e28050a67ec9042b7c578014f5ae2bc555b5df5a
David Yoon <qqpo12@naver.com>
2023-11-16 18:45:24 +0900

    Add a database
    * Added a mysql and a TypeOrmModule
    * Added a database config on the app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
